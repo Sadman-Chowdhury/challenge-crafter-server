7d07b9e4dd4794f05888d40180d0c5eb1d3b7375
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-06-13 00:16:18 +0600

    Basic Sculpture of the server

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+// -------------------middleware----------------------------------------------------------------
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// =================================================================
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.scfrsgh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // =================================================================
+
+    // =================================================================================================
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// =================================================================
+
+// ---------------------------------------------------------------------------------------------
+app.get("/", (req, res) => {
+  res.send("Challenge Crafter Server is Alive.............");
+});
+
+app.listen(port, () => {
+  console.log(`Challenge Crafter Server is running on port ${port}`);
+});
